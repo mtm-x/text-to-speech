2a7fae2929753d927fa83f5731978b7bc3ad7969
lotus <88881685+mtm-x@users.noreply.github.com>
2024-09-25 11:16:37 +0530

    Update t2s.py

--- t2s.py
+++ t2s.py
@@ -6,13 +6,10 @@ import os
 import time
 from PIL import Image
 import webbrowser
-
-
 app = tk.CTk()
 app.title("Text to Speech")
 app.geometry("600x460")
 tk.set_appearance_mode("Dark")
-
 image_path1 = os.path.join("assets", "play.png")
 play_image = tk.CTkImage(light_image=Image.open(image_path1), size=(30, 30))
 image_path2 = os.path.join("assets", "clear.png")
@@ -21,10 +18,8 @@ image_path3 = os.path.join("assets", "git.png")
 git_image = tk.CTkImage(light_image=Image.open(image_path3),size=(30,30))
 image_path4 = os.path.join("assets", "web.png")
 web_image = tk.CTkImage(light_image=Image.open(image_path4),size=(30,30))
-
 def clear():
     out.delete('0','end')
-
 def generate() :
     try :
         text = out.get()
@@ -49,17 +44,12 @@ def generate() :
     except :
         error = "something went wrong !! Try again"
         out.insert('0',error)
-
 def website():
     mtmsite= "https://mtm-x.github.io/"
     webbrowser.open_new(mtmsite)
-
 def web():
     git = "https://github.com/mtm-x"
     webbrowser.open_new(git)
-
-    
-
 out=tk.CTkEntry(app,
                   width=560,
                   height=100,
@@ -71,9 +61,6 @@ out=tk.CTkEntry(app,
                   
                   )
 out.place(x=20,y=20)
-
-
-
 my_clear = tk.CTkButton(app,
                         text="Clear",
                         command=clear,
@@ -88,8 +75,6 @@ my_clear = tk.CTkButton(app,
                         text_color="white",
                         )
 my_clear.place(x=380,y=180)
-
-
 convert=tk.CTkButton(app,
                          width=130,
                          height=60,
@@ -103,10 +88,7 @@ convert=tk.CTkButton(app,
                          text_color="white",
                          fg_color=("transparent"),
                          )
-
 convert.place(x=70,y=180)
-
-
 mtm=tk.CTkButton(app,
                  text="GitHub",
                  border_color="white",
@@ -120,7 +102,6 @@ mtm=tk.CTkButton(app,
                  fg_color=("transparent")
                 )
 mtm.place(x=70,y=290)
-
 myweb=tk.CTkButton(app,
                    text="Portfolio",
                    border_color="white",
@@ -133,7 +114,4 @@ myweb=tk.CTkButton(app,
                    command=website,
                    fg_color=("transparent"))
 myweb.place(x=70,y=370)
-
-
-
 app.mainloop()
